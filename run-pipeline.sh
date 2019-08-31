7b77451ae8be63f14743d35919ecb4040d2f7721
plantimals <5475390+plantimals@users.noreply.github.com>
2019-08-30 22:09:27 -0500

    add to pipeline script

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -2,3 +2,14 @@
 
 INPUT=$1
 
+REFERENCE=/path/to/ref.vcf.gz
+
+VCF_INPUT=/path/to/converted.vcf.gz
+
+# Stage 1 - convert input into VCF
+
+./2vcf conv h3africa --ref $REFERENCE --input $INPUT --output $VCF_INPUT
+
+# Stage 2 - phasing
+
+java -Xmx10g -jar beagle.24Aug19.3e8.jar -gt=$VCF_INPUT
