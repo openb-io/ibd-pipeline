e4c9e5b0933560e71399f81b55aab5f0479bf695
plantimals <5475390+plantimals@users.noreply.github.com>
2019-09-01 09:47:55 -0500

    functioning first stage

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-INPUT=$1
-
-REFERENCE=/path/to/ref.vcf.gz
-
-VCF_INPUT=/path/to/converted.vcf.gz
+echo "start"
 
 # Stage 1 - convert input into VCF
+function ingestion () {
+  SUBJECT=$1
+  SUBJECT_NAME=`basename ${SUBJECT} ".zip"`
+  SUBJECT_VCF="${WORKING_DIRECTORY}/${SUBJECT_NAME}.vcf.gz"
+  CMD="${TO_VCF} conv 23andme --ref ${REFERENCE} --input ${SUBJECT} --output ${SUBJECT_VCF}"
+  echo "about to run: ${CMD}"
+  eval ${CMD}
+}
 
-./2vcf conv h3africa --ref $REFERENCE --input $INPUT --output $VCF_INPUT
+echo "function defined"
 
-# Stage 2 - phasing
+TO_VCF=/Users/rob/src/github.com/plantimals/2vcf/bin/2vcf
 
-java -Xmx10g -jar beagle.24Aug19.3e8.jar -gt=$VCF_INPUT
+INPUT_LIST=$1
+
+REFERENCE=/Users/rob/data/science/markers/dbSNP/2vcf-v2.1.vcf.gz
+
+WORKING_DIRECTORY=./tmp/
+
+echo "constants setup"
+
+for SUBJECT in $(cat ${INPUT_LIST})
+do
+  echo "running ingestion(${SUBJECT})"
+  ingestion ${SUBJECT}
+done
