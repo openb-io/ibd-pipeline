50906fd203ef34a8144dc9561af071fc919d45bd
plantimals <5475390+plantimals@users.noreply.github.com>
2019-09-05 07:47:44 -0500

    hold pipeline changes

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,15 +1,104 @@
 #!/bin/bash
 
-INPUT=$1
+#list of paths to genotype array data 
+INPUT_LIST=$1
 
-REFERENCE=/path/to/ref.vcf.gz
+#path to .ped file for beagle4 use
+PEDIGREE=$2
 
-VCF_INPUT=/path/to/converted.vcf.gz
+#path to intermediate and result files
+WORKING_DIRECTORY=./tmp
+
+#path to binaries
+BIN_DIRECTORY=./bin
+
+#unique identifier for a pipeline run, signature of inputs, the MD5 hash of the input list file
+INPUT_MD5=`md5 -q ${INPUT_LIST}`
+
+#path to the 2vcf binary
+TO_VCF="${BIN_DIRECTORY}/2vcf"
+
+#path to 2vcf reference
+REFERENCE="${BIN_DIRECTORY}/2vcf-v2.1.vcf.gz"
+
+#path to beagle4 jar
+BEAGLE4_JAR="${BIN_DIRECTORY}/beagle.r1399.jar"
+
+#path to beagle5 jar
+BEAGLE5_JAR="${BIN_DIRECTORY}/beagle.24Aug19.3e8.jar"
+
+#path to refined IBD jar
+REFINED_IBD_JAR="${BIN_DIRECTORY}/refined-ibd.16May19.ad5.jar"
+
+#path to merged VCF after conversion of inputs
+MERGED_VCF="${WORKING_DIRECTORY}/${INPUT_MD5}-merged.vcf.gz"
+
+#path to phased VCF
+PHASED_VCF="${WORKING_DIRECTORY}/${INPUT_MD5}-merged-phased"
+
+#path to output VCF of IBD calling
+PHASED_IBD_OUT="${WORKING_DIRECTORY}/${INPUT_MD5}-merged-phased-ibd-called.vcf.gz"
+
+
+function indexVCF () {
+  local VCF=$1 
+  tabix ${VCF}
+}
+
+function getVCFName () {
+  local SUBJECT=$1
+  local SUBJECT_NAME=`basename ${SUBJECT} ".zip"`
+  VCF_PATH="${WORKING_DIRECTORY}/${SUBJECT_NAME}.vcf.gz"
+}
+
+# convert incoming genotype array calls to vcf
+function convert2VCF () {
+  local SUBJECT=$1 
+  getVCFName ${SUBJECT}
+  echo ${SUBJECT}
+  local CMD="${TO_VCF} conv 23andme --ref ${REFERENCE} --input ${SUBJECT} --output ${VCF_PATH} --fixAllos"
+  eval ${CMD}
+  indexVCF ${VCF_PATH}
+}
 
 # Stage 1 - convert input into VCF
+function ingestion () {
+  for SUBJECT in $(cat ${INPUT_LIST})
+  do
+    convert2VCF ${SUBJECT}
+  done
+}
 
-./2vcf conv h3africa --ref $REFERENCE --input $INPUT --output $VCF_INPUT
+# Stage 2 - merge VCFs
+function merge () {
+  local CMD="bcftools merge -O z -o ${MERGED_VCF}"
+  for SUBJECT in $(cat ${INPUT_LIST})
+  do 
+    getVCFName ${SUBJECT}
+    CMD="${CMD} ${VCF_PATH}"
+  done
+  eval "${CMD}"
+  indexVCF ${MERGED_VCF}
+}
 
-# Stage 2 - phasing
+# Stage 3 - phasing
+function phasingB4 () {
+  CMD="java -Xmx10g -jar ${BEAGLE4_JAR} nthreads=6 gt=${MERGED_VCF} impute=false gprobs=false ped=${PEDIGREE} ibd=true ibdlod=4.0 out=${PHASED_VCF}"
+  eval ${CMD}
+}
 
-java -Xmx10g -jar beagle.24Aug19.3e8.jar -gt=$VCF_INPUT
+function phasingB5 () {
+  CMD="java -Xmx10g -jar ${BEAGLE5_JAR} nthreads=4 gt=${MERGED_VCF} impute=false out=${PHASED_VCF}"
+  eval ${CMD}
+}
+
+function refinedIBD () {
+  CMD="java -Xmx10g -jar ${REFINED_IBD_JAR} nthreads=6 gt=${PHASED_VCF}.vcf.gz.vcf.gz.vcf.gz out=${PHASED_IBD_OUT}"
+  eval ${CMD}
+}
+
+# main pipeline
+
+# run stage 1 - convert input to VCF
+
+ingestion
